b30fa29bc29c7e0d5963f4dc9456b3a9ff5da246
nanwu <nanwu2014@u.northwestern.edu>
2016-01-21 23:47:08 -0500

    readme

--- __main__.py
+++ __main__.py
@@ -2,8 +2,8 @@
 from .client import Client
 
 cli = Client('127.0.01', 2080)
-cli.connect(('www.google.com', 80))
+cli.connect(('www.qq.com', 80))
 cli.sendall(
     b'GET / HTTP/1.1\n'
-    b'Host: www.google.com\n\n')
+    b'Host: www.qq.com\n\n')
 print(cli.recvall())
